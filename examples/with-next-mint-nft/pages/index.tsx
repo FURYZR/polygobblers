6febdfed89abe1d5e983fc6c8452b69cd10b1e76
FURYZR <119222194+FURYZR@users.noreply.github.com>
2022-11-27 19:58:19 +0100

    Update index.tsx

--- examples/with-next-mint-nft/pages/index.tsx
+++ examples/with-next-mint-nft/pages/index.tsx
@@ -13,7 +13,7 @@ import { abi } from '../contract-abi';
 import FlipCard, { BackCard, FrontCard } from '../components/FlipCard';
 
 const contractConfig = {
-  address: '0x7320b71d19428e920fC0232efe8df89492061fd8',
+  address: '0x86fbbb1254c39602a7b067d5ae7e5c2bdfd61a30',
   abi,
 };
 
@@ -101,25 +101,11 @@ const Home: NextPage = () => {
         <div style={{ flex: '0 0 auto' }}>
           <FlipCard>
             <FrontCard isCardFlipped={isMinted}>
-              <Image
-                layout="responsive"
-                src="/nft.png"
-                width="500"
-                height="500"
-                alt="RainbowKit Demo NFT"
-              />
               <h1 style={{ marginTop: 24 }}>Rainbow NFT</h1>
               <ConnectButton />
             </FrontCard>
             <BackCard isCardFlipped={isMinted}>
               <div style={{ padding: 24 }}>
-                <Image
-                  src="/nft.png"
-                  width="80"
-                  height="80"
-                  alt="RainbowKit Demo NFT"
-                  style={{ borderRadius: 8 }}
-                />
                 <h2 style={{ marginTop: 24, marginBottom: 6 }}>NFT Minted!</h2>
                 <p style={{ marginBottom: 24 }}>
                   Your NFT will show up in your wallet in the next few minutes.
